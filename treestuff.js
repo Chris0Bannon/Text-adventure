48562e435cb3f41a86e26cf458f867d2b552a50f
Christopher O'Bannon <Crobwan@gmail.com>
2019-07-10 18:06:02 -0500

    created audiogit statusgit status

--- treestuff.js
+++ treestuff.js
@@ -29,7 +29,7 @@
   plotMessage[0] = "idk my bff jill";
   plotMessage[1] = "just trying this out2";
   plotMessage[2] = "just trying this out3";
-  plotMessage[3] = "just trying this out4";
+  plotMessage[3] = "This area is overgrown with thorny brush that scraps and pulls at you as make your way forward.  You pull yourself into a small clearing that contains a hoveled yert.  a small coil of smoke seeps out from the dwellings roof.  It looks like someone may be home.";
   plotMessage[4] = "just trying this out5";
   plotMessage[5] = "just trying this out6";
   plotMessage[6] = "You awaken to warm and wet tongue licking your face.  You eyes peel open and are greeted with the slobbery smile of an incredibly large puppy.  You look around at your surroundings as you climb to seated position.  You find yourself in an expansive clearing surronded by a thick forestline bathed in moonlight. You are without your compass and map, but as a forest ranger you are without worry.  As you climb to your feet the pup playfully runs into the treeline to your north west.  The pup pauses to look back to you before dissapearing into the shadows of the forest.";
@@ -41,10 +41,10 @@
   blockedPathMessages[0] = "You can't go that way!";
   blockedPathMessages[1] = "Don'go north anymore, there is nothing cool over there.";
   blockedPathMessages[2] = "Idk, the game is supposed to end here but I dont know how to do that yet.";
-  blockedPathMessages[3] = "You like, cant head any further west, because of some plot reason, or something..";
+  blockedPathMessages[3] = "The undergrowth surounding the Hermits shack is way to thick for you to make your way through.  You'll have to go out the way that you came in.";
   blockedPathMessages[4] = "This code shouldn't actually appear in game until I get better at path blocking logic.";
   blockedPathMessages[5] = "Don't go West anymore, it sucks over there.";
-  blockedPathMessages[6] = "You should really stick with the puppy.  he is probably a plot device of some sort to guide your adventure.  Try heading north or east to follow him.";
+  blockedPathMessages[6] = "It seemed like the pup wants you to follow it.  Follow the pup into forest to the North or East to start your adventure!" ;
   blockedPathMessages[7] = "you're definetly headed the wrong way.  Try something else.";
   blockedPathMessages[8] = "Careful!  The edge of the earth is over there! Dont fall off!";
 
@@ -53,13 +53,16 @@
   dialogue[0] = "this is some sample dialogue."
   dialogue[1] = "this is some sample dialogue."
   dialogue[2] = "this is some sample dialogue."
-  dialogue[3] = "this is some sample dialogue."
+  dialogue[3] = "An old and weatherd face pokes out from the yert's flap.  The eyes of an old bearded man fall upon you as you clambers from the yert's entrance.  He turns to you and says 'I dont know what this character will say yet and havent worked on my dialogue arrays enough to have a meaningful conversation'. "
   dialogue[4] = "this is some sample dialogue."
   dialogue[5] = "this is some sample dialogue."
-  dialogue[6] = "You call out to the pup, but it is already gone."
+  dialogue[6] = "You call out after the pup, but it has already disapeared into the treeline and does not respond."
   dialogue[7] = "this is some sample dialogue."
   dialogue[8] = "this is some sample dialogue."
 
+//game audio
+let audios = [];
+audios[0] = "treesong.mp3"
 
   // set players start locations
   let mapLocation = 6;
@@ -84,10 +87,13 @@
   //The images
   let image = document.querySelector("img");
 
+  let audio = document.querySelector("audio");
+
   //The button
   let button = document.querySelector("button");
   button.style.cursor = "pointer";
   button.addEventListener("click", clickHandler, false);
+
 
   //Display the players location
   render();
@@ -191,4 +197,7 @@
 
   //Display the gameMessage
   outputMessage.innerHTML = "<textarea id = gameMessage>" + gameMessage + "</textarea>";
+
+  //output audio
+  audio.src = "audios/" + audios[0];
 }
